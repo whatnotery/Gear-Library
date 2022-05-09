181b38d16b826df922603804ce6d8443309a2903
Jos O'shea <jos@joshea.dev>
2022-05-08 23:36:29 -0400

    changes to view and action mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => "https://gear-library-production.up.railway.app/" }
+  config.action_mailer.default_url_options = { :host => "https://ky-bikepacking-gear-library.up.railway.app/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
