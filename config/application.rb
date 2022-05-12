d9003fecff84911b7cb7f29804b8461afab948c4
Jos O'shea <jos@joshea.dev>
2022-05-11 23:27:24 -0400

    last commit missed some things

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module GearLibrary
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    # 
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
